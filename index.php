d80e06d94b58e6be38d16dfdcc2d07cd709121e2
André Rafael Pinto Xavier Pais <andrerpxp@gmail.com>
2013-05-10 10:38:28 -0300

    Mudanças no Index e 404

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-$pageTitle = ""; // Até 65 caracteres do título.
-$pageDescription = ""; // Até 65 caracteres do título.
+$pageTitle = ""; // Até 90 mas o google corta em 63 caracteres do título.
+$pageDescription = ""; // Até 250 mas o google corta em 152 caracteres do título.
 $url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 $section = "index"; // Colocar o nome da URL (página) que será ativada, para mudar o menu.
 include('inc/header.php'); ?>
