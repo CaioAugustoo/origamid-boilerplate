a5b1fbaa5eed52807af2f34d8e6de89c468a224c
André Rafael Pinto Xavier Pais <andre@origamid.com>
2013-11-07 10:02:34 -0200

    Atualizado para o Boilerplate 4.3, adicionado os plugins Visibility e Waypoint, Atualizado o Normalize, Atualizadas as Task de Grunt, Atualizado o htaccess

--- inc/header.php
+++ inc/header.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>				<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>					<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>					<html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->	<html class="no-js" lang="pt-br"> <!--<![endif]-->
+<!--[if IE]>	<html class="no-js ie"> <![endif]-->
+<!--[if !IE]><!-->	<html class="no-js" lang="pt-br"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	
 	<title><?php echo $pageTitle; ?></title>
 	
@@ -15,7 +13,7 @@
 	<meta property="og:image" content=""/>
 	<meta property="og:site_name" content=""/>
 	<meta property="og:description" content="<?php echo $pageDescription; ?>"/>
-	<meta property="fb:page_id" content="" />
+
 	<meta name="description" content="<?php echo $pageDescription; ?>">
 	<meta name="keywords" content=""/>
 	<meta name="author" content="Origamid"/>
@@ -23,17 +21,11 @@
 	
 	<link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml">
 
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<link rel="shortcut icon" href="favicon.ico">
-	<link rel="apple-touch-icon" href="img/apple/apple-touch-icon.png">
-	<link rel="apple-touch-icon" href="img/apple/apple-touch-icon-precomposed.png">
-	<link rel="apple-touch-icon" sizes="57x57" href="img/apple/apple-touch-icon-57x57-precomposed.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="img/apple/apple-touch-icon-72x72-precomposed.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="img/apple/apple-touch-icon-114x114-precomposed.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="img/apple/apple-touch-icon-144x144-precomposed.png">
 	
 	<link rel="stylesheet" href="style.css">
-	<script src="js/vendor/modernizr.custom.51217.js"></script>
+	<script src="js/vendor/modernizr.custom.68175.js"></script>
 </head>
 <body>
